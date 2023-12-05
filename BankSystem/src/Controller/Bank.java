b116d087a6967d1f0d65a2d2075bf21162077505
Karan Desai <kingstyle075@gmail.com>
2023-12-06 00:52:10 +0530

    JDBC project
    JDBC program

---
+++ BankSystem/src/Controller/Bank.java
@@ -0,0 +1,15 @@
+package Controller;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Service sc = new Service();
+//		while(true) {
+//			sc.displayMenu();
+//		}
+		BankDb d = new BankDb();
+		d.dispalyAcc();
+	}
+}
